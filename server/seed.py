549658fe0074c8e6fc06ae366893121e7cab3234
Your Name <usamax089@gmail.com>
2025-01-17 17:17:16 +0300

    complete

--- server/seed.py
+++ server/seed.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 #server/seed.py
+from random import choice as rc 
+from faker import Faker 
+
 
 from app import app
 from models import db, Pet
 
 with app.app_context():
+    
+    
+    faker=Faker()
+    
+    
+    Pet.query.delete()
 
     # Create an empty list
     pets = []
 
     # Add some Pet instances to the list
-    pets.append(Pet(name = "Fido", species = "Dog"))
-    pets.append(Pet(name = "Whiskers", species = "Cat"))
-    pets.append(Pet(name = "Hermie", species = "Hamster"))
-
+    species = ['Dog', 'Cat', 'human']
+    
+    for n in range(10):
+        pet = Pet(name=faker.first_name(), species=rc(species))
+        pets.append(pet)
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
 
